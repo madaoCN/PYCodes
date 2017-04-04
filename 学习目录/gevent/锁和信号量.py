fafb95028fb58da18b26c9a4e87d643d2a570d33
梁宪松 <1349963838@qq.com>
2017-04-04 10:41:21 +0800

    【更新】自然语言学习

---
+++ 学习目录/gevent/锁和信号量.py
@@ -0,0 +1,25 @@
+from gevent import sleep
+from gevent.pool import Pool
+from gevent._semaphore import BoundedSemaphore
+
+sem = BoundedSemaphore(2)
+
+def worker1(n):
+    sem.acquire()
+    print('Worker %i acquired semaphore' % n)
+    sleep(0)
+    sem.release()
+    print('Worker %i released semaphore' % n)
+
+def worker2(n):
+    with sem:
+        print('Worker %i acquired semaphore' % n)
+    sleep(0)
+    print('Worker %i released semaphore' % n)
+
+pool = Pool()
+pool.map(worker1, range(0, 2))
+pool.map(worker2, range(3, 6))
+pool.join()
+
+
